b2ba4a2d6241635d30bf57a239ef6fd242a27ecc
Parth Sinha <sinhaparth555@gmail.com>
2025-08-25 14:09:07 +0100

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom'],
-          domtoimage: ['dom-to-image'],
+          domtoimagemore: ['dom-to-image-more'],
         },
       },
     },
